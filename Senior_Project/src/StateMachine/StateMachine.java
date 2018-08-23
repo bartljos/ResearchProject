6c5a952c6ef83aba424791e11143042dc5874c02
Josh <jlbartle@hotmail.com>
2018-08-23 16:31:08 -0400

    some code

--- Senior_Project/src/StateMachine/StateMachine.java
+++ Senior_Project/src/StateMachine/StateMachine.java
@@ -1,5 +1,22 @@
 package StateMachine;
 
-public class StateMachine {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 
+public class StateMachine {
+	
+	ArrayList<String> dictionary = new ArrayList<String>();
+	
+	public boolean readDictionary()
+	{
+		FileReader fr = null;
+		try {
+			 fr = new FileReader("C:\\Users\\Josh\\Desktop\\SrRsrch Resources\\Dictionary\\wordlist.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 }
