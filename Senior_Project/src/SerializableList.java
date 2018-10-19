b7ddd41d5e23ffc9980169ca0cc29b0708a0e632
Josh <jlbartle@hotmail.com>
2018-10-19 06:32:36 -0400

    UPDATE

---
+++ Senior_Project/src/SerializableList.java
@@ -0,0 +1,41 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SerializableList implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	List<String> wordList = new ArrayList<String>();
+	List<Integer> occurences = new ArrayList<Integer>();
+
+	
+	public void addWord(String w)
+	{
+		int result = Collections.binarySearch(wordList, w);
+		
+		
+		
+		if(result < 0)
+		{
+			wordList.add((result+1) * -1, w);
+			occurences.add((result +1) * -1, 1);
+			
+		}else
+		{
+		
+	
+			occurences.set(result, occurences.get(result) + 1);
+		}
+		
+		
+	}
+	
+	public void printAll()
+	{
+		System.out.println(wordList.toString() + " ---> "	+ wordList.size());
+		System.out.println(occurences.toString() + " ---> "	+ occurences.size());
+	}
+	
+
+}
